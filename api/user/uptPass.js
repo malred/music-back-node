e5a1b7470c67699cf55ae27f95ada7a8689a0527
malred <99012268+malred@users.noreply.github.com>
2022-12-08 21:17:57 +0800

    Add files via upload

---
+++ api/user/uptPass.js
@@ -0,0 +1,25 @@
+import db from "../db/db";
+// 修改密码
+export default async function uptPass(req, res) {
+  if (null !== req && undefined !== res) {
+    const { uname, upass } = req.query;
+    await db
+      .query(
+        `update muser
+            set upass = '${upass}'
+            where uname = '${uname}'`
+      )
+      .then(() => {
+        return res.send({
+          status: 200,
+          data: "修改成功",
+        });
+      })
+      .catch(() => {
+        return res.send({
+          status: 200,
+          msg: "修改失败",
+        });
+      });
+  }
+}
