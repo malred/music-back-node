56585c3f1949a1a13e977706f7a2de41bc519455
malred <99012268+malred@users.noreply.github.com>
2022-12-08 22:26:03 +0800

    Update info.js

--- api/user/info.js
+++ api/user/info.js
@@ -29,7 +29,7 @@ export default async function getInfo(req, res) {
           if (rows.length > 0) {
             return res.send({
               status: 200,
-              data: rows,
+              data: JSON.parse(rows), 
             });
           } else {
             return res.send({
