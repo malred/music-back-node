d20f5e64e9590e1f34e8e48e030bd0d736013878
malred <99012268+malred@users.noreply.github.com>
2022-12-09 11:49:26 +0800

    Update uptMsg.js

--- api/user/uptMsg.js
+++ api/user/uptMsg.js
@@ -2,7 +2,7 @@ import db from "../db/db";
 // 修改密码
 export default async function uptMsg(req, res) {
   if (null !== req && undefined !== req) {
-    const { id, name, age, sex, birth, location } = req.body;
+    const { id, name, age, sex, birth, location } = req.body.data;
     await db
       .query(
         ` update music.muser_info
