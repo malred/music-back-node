fd5ed1ded03d13ebdb61b49165c48451334db4ea
malred <99012268+malred@users.noreply.github.com>
2022-12-09 13:24:53 +0800

    Update uptMsg.js

--- api/user/uptMsg.js
+++ api/user/uptMsg.js
@@ -1,29 +1,32 @@
 import db from "../db/db";
 // 修改信息
 export default async function uptMsg(req, res) {
-  if (null !== req && undefined !== req) {
-    const { id, name, age, sex, birth, location } = req.body.data;
-    await db
-      .query(
-        ` update music.muser_info
-          set   name= '${name}',
-                age ='${age}',
-                sex='${sex}',
-                birth=DATE_FORMAT('${birth}', '%Y-%m-%d'),
-                location='${location}'
-          where id = '${id}';`
-      )
-      .then(() => {
-        return res.send({
-          status: 200,
-          data: "修改成功",
-        });
-      })
-      .catch(() => {
-        return res.send({
-          status: 200,
-          msg: "修改失败",
-        });
-      });
+  if (!req || !req.body) {
+    return res.send({
+      status: 400,
+      msg: "请求参数错误",
+    });
+  }
+
+  const { id, name, age, sex, birth, location } = req.body;
+  try {
+    await db.query(
+      `update music.muser_info
+       set name = '${name}',
+           age = '${age}',
+           sex = '${sex}',
+           birth = DATE_FORMAT('${birth}', '%Y-%m-%d'),
+           location = '${location}'
+       where id = '${id}';`
+    );
+    return res.send({
+      status: 200,
+      data: "修改成功",
+    });
+  } catch (error) {
+    return res.send({
+      status: 500,
+      msg: "修改失败",
+    });
   }
 }
