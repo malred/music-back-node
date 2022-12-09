ff0f1d43a690c4b34247d2a56969fcf1ba4db34b
malred <99012268+malred@users.noreply.github.com>
2022-12-09 11:50:10 +0800

    Update uptMsg.js

--- api/user/uptMsg.js
+++ api/user/uptMsg.js
@@ -1,17 +1,17 @@
-import db from "../db/db"; 
-// 修改密码
+import db from "../db/db";
+// 修改信息
 export default async function uptMsg(req, res) {
   if (null !== req && undefined !== req) {
     const { id, name, age, sex, birth, location } = req.body.data;
     await db
       .query(
         ` update music.muser_info
-          set   name= ${name},
-                age = ${age},
-                sex=${sex},
-                birth=DATE_FORMAT(${birth}, '%Y-%m-%d'),
-                location=${location}
-                where id = ${id};`
+          set   name= '${name}',
+                age ='${age}',
+                sex='${sex}',
+                birth=DATE_FORMAT('${birth}', '%Y-%m-%d'),
+                location='${location}'
+          where id = '${id}';`
       )
       .then(() => {
         return res.send({
