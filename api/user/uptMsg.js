a5c52145c3bfd935e2dce7ff1b62b9bd62e1553e
malred <99012268+malred@users.noreply.github.com>
2022-12-09 09:00:21 +0800

    Add files via upload

---
+++ api/user/uptMsg.js
@@ -0,0 +1,29 @@
+import db from "../db/db";
+// 修改密码
+export default async function uptMsg(req, res) {
+  if (null !== req && undefined !== res) {
+    const { id, name, age, sex, birth, location } = req.query;
+    await db
+      .query(
+        ` update muser_info
+          set   name= ${name},
+                age = ${age},
+                sex=${sex},
+                birth=DATE_FORMAT(${birth}, '%Y-%m-%d'),
+                location=${location}
+                where id = ${id};`
+      )
+      .then(() => {
+        return res.send({
+          status: 200,
+          data: "修改成功",
+        });
+      })
+      .catch(() => {
+        return res.send({
+          status: 200,
+          msg: "修改失败",
+        });
+      });
+  }
+}
