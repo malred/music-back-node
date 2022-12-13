458d992a292540ba3c801e7dc246b6b947f3927e
malred <99012268+malred@users.noreply.github.com>
2022-12-13 22:03:00 +0800

    Update login.js

--- api/user/login.js
+++ api/user/login.js
@@ -1,5 +1,7 @@
-import db from "../../utils/db";
-import R from '../../utils/res'
+// import db from "../../utils/db";
+const db = require('../../utils/db')
+// import R from '../../utils/res'
+const R = require('../../utils/res')
 /** 数据库查询,登录 */
 export default function getUserByUnameAndUpass(uname, upass, res) {
     try {
